073815a3548c142b92e8fadf1f095bc29899ba00
DC <eopoxf@gmail.com>
2020-02-08 18:14:58 +0530

    cleanup

--- src/components/screen-popup/screen-popup.js
+++ src/components/screen-popup/screen-popup.js
@@ -1,6 +1,6 @@
-import {bindable, customElement} from 'aurelia-framework';
-import { Config } from "resources/config";
-import { inject, observable } from "aurelia-framework";
+import { bindable, customElement } from 'aurelia-framework';
+import { Config } from 'resources/config';
+import { inject, observable } from 'aurelia-framework';
 
 
 //start-aurelia-decorators
@@ -24,36 +24,31 @@ export class ScreenPopup {
   }
 
   queryChanged(newval, oldval) {
-
     $('#dropdown_city').on('click', function() {
       $(this).toggleClass('clicked');
-  });
+    });
     this.searchText = newval.toLowerCase();
-    console.log(this.searchText)
     // if(this.searchText.length > 0){
     //   $('#dropdown_city').show();
     // }else{
     //   $('#dropdown_city').hide();
     // }
-    console.log(this.config)
 
     const map = Object.keys(this.config.instance_regions);
-    console.log(map)
+    // console.log(map)
     let newObj = map.filter(value => {
-      return value.indexOf(newval.toLowerCase()) != -1 ? value : null;
+      return value.indexOf(newval.toLowerCase()) !== -1 ? value : null;
     });
     this.searchResult = newObj;
-    if(this.searchResult <= 0){
+    if (this.searchResult <= 0) {
       $('#dropdown_city').hide();
     }
-    console.log(this.searchResult);
   }
 
 
   switchCity(city) {
-    console.log(city)
     this.changeCity(city, true);
-      $("#screen").css("display", "none");;
+    $('#screen').css('display', 'none');
 
   }
   attached() {
